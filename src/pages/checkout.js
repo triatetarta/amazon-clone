505dbe45dc796c9db21b046202d4a109f0d9e01a
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-05-25 20:27:54 +0100

    products total

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,16 @@
 import Header from '../components/Header';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
-import { selectItems } from '../slices/basketSlice';
+import { selectItems, selectTotal } from '../slices/basketSlice';
 import CheckoutProduct from '../components/CheckoutProduct';
+import Currency from 'react-currency-formatter';
+import { useSession } from 'next-auth/client';
 
 const Checkout = () => {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const [session] = useSession();
+
   return (
     <div className='bg-gray-50'>
       <Header />
@@ -25,14 +30,34 @@ const Checkout = () => {
                 ? 'Your Amazon Basket is empty'
                 : 'Shopping Basket'}
             </h1>
-            {items.map((item) => (
-              <CheckoutProduct key={item.id} {...item} />
+            {items.map((item, i) => (
+              <CheckoutProduct key={i} {...item} />
             ))}
           </div>
         </div>
 
         {/* right */}
-        <div></div>
+        <div className='flex flex-col bg-white p-10 shadow-md'>
+          {items.length > 0 && (
+            <>
+              <h2 className='whitespace-nowrap'>
+                Subtotal ({items.length} items):{' '}
+                <span className='font-bold'>
+                  <Currency quantity={total} currency='GBP' />
+                </span>
+              </h2>
+
+              <button
+                className={`button mt-2 ${
+                  !session &&
+                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+                }`}
+              >
+                {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
